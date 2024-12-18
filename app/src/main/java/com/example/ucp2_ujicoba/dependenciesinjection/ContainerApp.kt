de75d1cdfd8898f181d90dc1fe29b5c2577a8199
bimbim1214 <bimoadityapangestu@gmail.com>
2024-12-18 22:31:04 +0700

    Menambahkan kelas ContainerApp untuk menyediakan implementasi dependensi RepositoryBrg dan RepositorySpr.

---
+++ app/src/main/java/com/example/ucp2_ujicoba/dependenciesinjection/ContainerApp.kt
@@ -0,0 +1,19 @@
+package com.example.ucp2_ujicoba.dependenciesinjection
+
+import android.content.Context
+import com.example.ucp2_ujicoba.data.database.TokoDatabase
+import com.example.ucp2_ujicoba.repository.LocalRepositoryBrg
+import com.example.ucp2_ujicoba.repository.LocalRepositorySpr
+import com.example.ucp2_ujicoba.repository.RepositoryBrg
+import com.example.ucp2_ujicoba.repository.RepositorySpr
+
+
+
+class ContainerApp (private val context: Context) : InterfaceContainerApp{
+    override val repositoryBrg: RepositoryBrg by lazy {
+        LocalRepositoryBrg(TokoDatabase.getDatabase(context).barangDao())
+    }
+    override val repositorySpr: RepositorySpr by lazy {
+        LocalRepositorySpr(TokoDatabase.getDatabase(context).suplierDAo())
+    }
+}
