2226d999f4935995be982d3bfd6c3150dbc7887e
bimbim1214 <bimoadityapangestu@gmail.com>
2024-12-18 20:55:12 +0700

    membaut data class BarangEvent

---
+++ app/src/main/java/com/example/ucp2_ujicoba/ui/viewmodel/barang/BarangViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.ucp2_ujicoba.ui.viewmodel.barang
+
+
+
+data class BarangEvent(
+    val id: String = "",
+    val nama: String = "",
+    val Deskripsi: String = "",
+    val Harga: String = "",
+    val Stok: String = "",
+    val NamaSuplier: String = ""
+)
