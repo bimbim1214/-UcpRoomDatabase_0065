49ef234c1562697571b12c85f3202728c99f6bd0
bimbim1214 <bimoadityapangestu@gmail.com>
2024-12-18 22:35:09 +0700

    membuat data class HomeUIState

---
+++ app/src/main/java/com/example/ucp2_ujicoba/ui/viewmodel/barang/HomeBrgViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.ucp2_ujicoba.ui.viewmodel.barang
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2_ujicoba.data.entity.Barang
+import com.example.ucp2_ujicoba.repository.RepositoryBrg
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import java.lang.Error
+
+
+data class HomeUIState(
+    val listBrg: List<Barang> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
