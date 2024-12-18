3a030209910e5c4ce7ef64b27fcb664d4f3f5906
bimbim1214 <bimoadityapangestu@gmail.com>
2024-12-18 20:11:57 +0700

    membuat operasi query untuk melakukan CRUD

---
+++ app/src/main/java/com/example/ucp2_ujicoba/data/dao/BarangDao.kt
@@ -0,0 +1,26 @@
+package com.example.ucp2_ujicoba.data.dao
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.ucp2_ujicoba.data.entity.Barang
+import kotlinx.coroutines.flow.Flow
+
+interface BarangDao {
+
+    @Insert
+    suspend fun insertBarang(barang: Barang)
+
+    @Query("SELECT * FROM barang ORDER BY nama ASC")
+    fun getAllBarang(): Flow<List<Barang>>
+
+    @Query("SELECT * FROM barang WHERE id = :id")
+    fun getBarang(id: String): Flow<Barang>
+
+    @Delete
+    suspend fun deleteBarang(barang: Barang)
+
+    @Update
+    suspend fun updateBarang(barang: Barang)
+}
