24c156a998a50ed4a1d3996bbf8c68f59acec859
bimbim1214 <bimoadityapangestu@gmail.com>
2024-12-18 20:58:46 +0700

    Menambahkan fungsi ekstensi untuk mengonversi BarangEvent menjadi Barang.

--- app/src/main/java/com/example/ucp2_ujicoba/ui/viewmodel/barang/BarangViewModel.kt
+++ app/src/main/java/com/example/ucp2_ujicoba/ui/viewmodel/barang/BarangViewModel.kt
@@ -2,16 +2,14 @@ package com.example.ucp2_ujicoba.ui.viewmodel.barang
 
 import com.example.ucp2_ujicoba.data.entity.Barang
 
-
-    fun BarangEvent.toBarangEntity(): Barang = Barang(
-        id = id,
-        nama = nama,
-        Deskripsi = Deskripsi,
-        Harga = Harga,
-        Stok = Stok,
-        NamaSuplier = NamaSuplier
-    )
-
+fun BarangEvent.toBarangEntity(): Barang = Barang(
+    id = id,
+    nama = nama,
+    Deskripsi = Deskripsi,
+    Harga = Harga,
+    Stok = Stok,
+    NamaSuplier = NamaSuplier
+)
 data class BarangEvent(
     val id: String = "",
     val nama: String = "",
