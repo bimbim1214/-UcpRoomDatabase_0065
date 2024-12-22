d0926c52d0388689bc719e286aeb38c02ec817c1
bimbim1214 <bimoadityapangestu@gmail.com>
2024-12-23 02:45:38 +0700

    Menambahkan komponen ComponentDetailBrg untuk menampilkan detail barang dengan judul dan isinya.

--- app/src/main/java/com/example/ucp2_ujicoba/ui/view/barang/DetailBrgView.kt
+++ app/src/main/java/com/example/ucp2_ujicoba/ui/view/barang/DetailBrgView.kt
@@ -43,6 +43,32 @@ import com.example.ucp2_ujicoba.ui.viewmodel.suplier.DetailUiState
 
 
 @Composable
+fun ComponentDetailBrg(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String
+
+){
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+
+@Composable
 private fun DeleteConfirmationDialog(
     onDeleteConfirm: () -> Unit,
     onDeleteCencel: () -> Unit,
