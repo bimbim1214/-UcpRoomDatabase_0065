78f29015b779fec3d686c01722046c65eca08da3
bimbim1214 <bimoadityapangestu@gmail.com>
2024-12-22 23:22:53 +0700

    membaut data class SuplierEvent

---
+++ app/src/main/java/com/example/ucp2_ujicoba/ui/viewmodel/suplier/SuplierViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.ucp2_ujicoba.ui.viewmodel.suplier
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2_ujicoba.data.entity.Suplier
+import com.example.ucp2_ujicoba.repository.RepositorySpr
+import com.example.ucp2_ujicoba.ui.viewmodel.barang.BarangEvent
+import kotlinx.coroutines.launch
+
+
+data class SuplierEvent(
+    val id: String = "",
+    val nama: String = "",
+    val kontak: String = "",
+    val alamat: String = "",
+)
