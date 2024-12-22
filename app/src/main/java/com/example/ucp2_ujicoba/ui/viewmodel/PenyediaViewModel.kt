6ede5f7e58131fe240669162794f15e35406677b
bimbim1214 <bimoadityapangestu@gmail.com>
2024-12-23 01:39:50 +0700

    Menambahkan PenyediaViewModel object untuk menyediakan dan menginisialisasi berbagai ViewModel dengan dependensi yang diperlukan

--- app/src/main/java/com/example/ucp2_ujicoba/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/ucp2_ujicoba/ui/viewmodel/PenyediaViewModel.kt
@@ -15,6 +15,51 @@ import com.example.ucp2_ujicoba.ui.viewmodel.suplier.HomeSprViewModel
 import com.example.ucp2_ujicoba.ui.viewmodel.suplier.SuplierViewModel
 
 
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer {
+            BarangViewModel(
+                tokoApp().containerApp.repositoryBrg
+            )
+        }
+
+        initializer {
+            HomeBrgViewModel(
+                tokoApp().containerApp.repositoryBrg
+            )
+        }
+        initializer {
+            DetailBrgViewModel(
+                createSavedStateHandle(),
+                tokoApp().containerApp.repositoryBrg
+            )
+        }
+        initializer {
+            UpdateBrgViewModel(
+                createSavedStateHandle(),
+                tokoApp().containerApp.repositoryBrg
+            )
+        }
+        initializer {
+            SuplierViewModel(
+                tokoApp().containerApp.repositorySpr
+            )
+        }
+
+        initializer {
+            HomeSprViewModel(
+                tokoApp().containerApp.repositorySpr
+            )
+        }
+
+        initializer {
+            DetailSprViewModel(
+                createSavedStateHandle(),
+                tokoApp().containerApp.repositorySpr
+            )
+        }
+    }
+}
 
 fun CreationExtras.tokoApp(): TokoApp =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as TokoApp)
