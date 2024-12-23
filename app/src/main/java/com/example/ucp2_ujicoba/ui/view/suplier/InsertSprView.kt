fcb2709858901443e5eebd87c9053eae97973730
bimbim1214 <bimoadityapangestu@gmail.com>
2024-12-23 16:48:19 +0700

    Menambahkan komponen FormSuplier untuk input data supplier dengan validasi dan penanganan error

---
+++ app/src/main/java/com/example/ucp2_ujicoba/ui/view/suplier/InsertSprView.kt
@@ -0,0 +1,108 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2_ujicoba.ui.customwidget.AppBar
+import com.example.ucp2_ujicoba.ui.navigation.AlamatNavigasi
+import com.example.ucp2_ujicoba.ui.viewmodel.PenyediaViewModel
+import com.example.ucp2_ujicoba.ui.viewmodel.suplier.FormErrorStateSpr
+import com.example.ucp2_ujicoba.ui.viewmodel.suplier.SprUIState
+import com.example.ucp2_ujicoba.ui.viewmodel.suplier.SuplierEvent
+import com.example.ucp2_ujicoba.ui.viewmodel.suplier.SuplierViewModel
+import kotlinx.coroutines.launch
+
+
+
+
+@Composable
+fun FormSuplier(
+    suplierEvent: SuplierEvent = SuplierEvent(),
+    onValueChange: (SuplierEvent) -> Unit = {},
+    errorState: FormErrorStateSpr = FormErrorStateSpr(),
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        // Input untuk Nama Barang
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = suplierEvent.id,
+            onValueChange = {
+                onValueChange(suplierEvent.copy(id = it))
+            },
+            label = { Text(text = "ID") },
+            isError = errorState.id != null,
+            placeholder = { Text(text = "Masukkan ID") }
+        )
+        Text(
+            text = errorState.id ?: "",
+            color = Color.Red
+        )
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = suplierEvent.nama,
+            onValueChange = {
+                onValueChange(suplierEvent.copy(nama = it))
+            },
+            label = { Text(text = "Nama") },
+            isError = errorState.nama != null,
+            placeholder = { Text(text = "Masukkan nama") }
+        )
+        Text(
+            text = errorState.nama ?: "",
+            color = Color.Red
+        )
+
+        // Input untuk Harga Barang
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = suplierEvent.alamat,
+            onValueChange = {
+                onValueChange(suplierEvent.copy(alamat = it))
+            },
+            label = { Text(text = "Alamat") },
+            isError = errorState.alamat != null,
+            placeholder = { Text(text = "Masukkan alamat") }
+        )
+        Text(
+            text = errorState.alamat ?: "",
+            color = Color.Red
+        )
+
+        // Input untuk Stok Barang
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = suplierEvent.kontak,
+            onValueChange = {
+                onValueChange(suplierEvent.copy(kontak = it))
+            },
+            label = { Text(text = "Kontak") },
+            isError = errorState.kontak != null,
+            placeholder = { Text(text = "Masukkan kontak") }
+        )
+        Text(
+            text = errorState.kontak ?: "",
+            color = Color.Red
+        )
+
+    }
+}
+
+
